6fb0714c73058346f162142ce665dbada8b104d6
EdPaul1 <saidarajab101@gmail.com>
2023-05-25 21:50:24 +0300

    Triangle classification

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,24 @@
 class Triangle
-  # write code here
+  class TriangleError < StandardError; end
+
+  def initialize(side1, side2, side3)
+    @sides = [side1, side2, side3]
+    validate_triangle
+  end
+
+  def kind
+    if @sides.uniq.length == 1
+      :equilateral
+    elsif @sides.uniq.length == 2
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    raise TriangleError if @sides.min <= 0 || @sides.sum - 2 * @sides.max <= 0
+  end
 end
